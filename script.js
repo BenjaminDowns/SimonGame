f703b52da5dae9ce6815f40fa8320b15d5899119
Benjamin Downs <benjamin.downs@gmail.com>
2016-03-08 20:27:14 +0100

    working on logic

--- script.js
+++ script.js
@@ -10,7 +10,8 @@
     let togglePower = document.getElementById('toggle-switch')
     let startButton = document.getElementById('start');
     let resetButton = document.getElementById('reset');
-
+    let tracker = 0;
+    
     const SIMON = {
 
         moves: [],
@@ -24,9 +25,10 @@
             startButton.disabled = !startButton.disabled
             resetButton.disabled = !resetButton.disabled
         },
-        addAMove() {  
-            this.moves.push(this.pieces[Math.floor(Math.random() * 4)])
-            console.log(this.moves)
+        addAMove() { 
+            let newPiece = this.pieces[Math.floor(Math.random() * 4)]
+            newPiece.setAttribute('AI', true)
+            this.moves.push(newPiece)
         },
         wait() {
             console.log('waiting now');
@@ -35,7 +37,8 @@
             SIMON.addAMove();
             for (let start = 0; start <= SIMON.moves.length - 1; start++) {
                 setTimeout(() => {
-                    SIMON.moves[start].click({ AI: true })
+                    SIMON.moves[start].click()
+                    tracker++
                 }, start * 1000)
             }
         },
@@ -68,28 +71,25 @@
     g.addEventListener('click', lightUp)
     y.addEventListener('click', lightUp)
     b.addEventListener('click', lightUp)
-    
-    let turn = 0
-    let index = 0
+    let move = 0
     let counter = 1;
     function lightUp(e) {
-        turn++
-        counter *= turn
-        console.log(index)
-        if (counter % 2 === 0) {
-            PLAYER.moves.push(e.target.id)
-            console.log(PLAYER.moves)
-            console.log(SIMON.moves)
-            
-            console.log(PLAYER.moves[index] === SIMON.moves[index]['id'])
+        move++
+        if (e.path[0].attributes.ai && e.path[0].attributes.ai.value !== 'undefined') {
+            console.log('AI turn')
+            counter++
+            e.path[0].attributes.ai.value = undefined
         }
-        index++
-        // if (e !== null) {
-        //     if (e.target === controls || e.target === togglePower) {
-        //         e.stopPropagation();
-        //         return null;
-        //     }
-
+        
+        if (counter % 2 === 0) {
+            console.log('player turn')
+            // PLAYER.moves.push(e.target.id)
+            // console.log(PLAYER.moves)
+            // console.log(SIMON.moves)
+            
+            // console.log(PLAYER.moves[index] === SIMON.moves[index]['id'])
+        }
+        
         let clickedItem = e.target
         let original = clickedItem.style.background
         clickedItem.style.background = 'lightgray'
@@ -97,12 +97,13 @@
         setTimeout(function() {
             clickedItem.style.background = original
         }, 300)
+        
     }
     // } // end lightup function
 
     // setInterval(() => {
-    //     if (PLAYER.moves.length === SIMON.moves.length)
-    //         SIMON.simulate();
+    //     if (tracker == SIMON.moves.length && tracker > 0)
+    //         console.log("PLAYER'S TURN")
     // }, 500)
 
 
